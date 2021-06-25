d5eb6e547d5dc9b0593517cd3932f6c608157c7e
Gabriel Paiva <gabriel.alcantara.ti@gmail.com>
2021-06-25 15:19:10 -0300

    A aplicação

---
+++ src/screens/Guilds/index.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { FlatList, View } from 'react-native';
+
+import { Guild, GuildProps } from '../../components/Guild';
+import { ListDivider } from '../../components/ListDivider';
+
+
+import { styles } from './styles';
+
+type Props = {
+    handleGuildSelect: (guild: GuildProps) => void;
+}
+
+export function Guilds({ handleGuildSelect }: Props) {
+    const guilds = [
+        {
+            id: '1',
+            name: 'Lendarios',
+            icon: 'image.png',
+            owner: true
+        },
+        {
+            id: '2',
+            name: 'Galera do game',
+            icon: 'image.png',
+            owner: true
+        },
+        {
+            id: '3',
+            name: 'Galera do game',
+            icon: 'image.png',
+            owner: true
+        },
+        {
+            id: '4',
+            name: 'Galera do game',
+            icon: 'image.png',
+            owner: true
+        },
+        {
+            id: '5',
+            name: 'Galera do game',
+            icon: 'image.png',
+            owner: true
+        },
+        {
+            id: '7',
+            name: 'Galera do game',
+            icon: 'image.png',
+            owner: true
+        },
+        {
+            id: '8',
+            name: 'Galera do game',
+            icon: 'image.png',
+            owner: true
+        },
+        {
+            id: '9',
+            name: 'Galera do game',
+            icon: 'image.png',
+            owner: true
+        },
+        {
+            id: '6',
+            name: 'Galera do game',
+            icon: 'image.png',
+            owner: true
+        },
+    ];
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={guilds}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <Guild
+                        data={item}
+                        onPress={() => handleGuildSelect(item)}
+                    />
+                )}
+                showsVerticalScrollIndicator={false}
+                ItemSeparatorComponent={() => <ListDivider isCentered />}
+                contentContainerStyle={{ paddingBottom: 68, paddingTop: 104 }}
+                ListHeaderComponent={() => <ListDivider isCentered />}
+                style={styles.guilds}
+            />
+        </View>
+    )
+
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { View, FlatList } from 'react-native';
+
+import { CategorySelect } from '../../components/CategorySelect';
+import { ButtonAdd } from '../../components/ButtonAdd';
+import { Profile } from '../../components/Profile';
+import { ListHeader } from '../../components/ListHeader';
+import { Appointment } from '../../components/Appointment';
+import { ListDivider } from '../../components/ListDivider';
+import { Background } from '../../components/Background';
+import { useNavigation } from '@react-navigation/core';
+
+
+import { styles } from './styles';
+
+
+export function Home() {
+    const [category, setCategory] = useState('');
+
+    const navigation = useNavigation();
+
+    const appointments = [
+        {
+            id: '1',
+            guild: {
+                id: '1',
+                name: 'Lendarios',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+        },
+        {
+            id: '2',
+            guild: {
+                id: '1',
+                name: 'Lendarios',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+        },
+    ]
+    function handleCategorySelect(categoryId: string) {
+        categoryId === category ? setCategory('') : setCategory(categoryId);
+    }
+
+    function handleAppointmentDetails() {
+        navigation.navigate('AppointmentDetails');
+    }
+
+    function handleAppointmentCreate() {
+        navigation.navigate('AppointmentCreate');
+    }
+
+    return (
+        <Background>
+            <View style={styles.header}>
+                <Profile />
+                <ButtonAdd onPress={handleAppointmentCreate} />
+            </View>
+
+            <CategorySelect
+                hasCheckBox={false}
+                categorySelected={category}
+                setCategory={handleCategorySelect}
+            />
+
+            <ListHeader
+                title="Partidas agendadas"
+                subtitle="Total 6"
+            />
+            <FlatList
+                data={appointments}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <Appointment
+                        data={item}
+                        onPress={handleAppointmentDetails}
+                    />
+                )}
+                ItemSeparatorComponent={() => <ListDivider />}
+                contentContainerStyle={{ paddingBottom: 69 }}
+                style={styles.matches}
+                showsVerticalScrollIndicator={false}
+            />
+        </Background>
+    );
+
+}
